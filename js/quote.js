4e5a2fbcbea9d62a31c5c01478213f2cbfcf85c5
Yana Sebrukovich <85887443+YaninaSeb@users.noreply.github.com>
2022-03-21 15:43:58 +0300

    feat: add Russian quotes

--- js/quote.js
+++ js/quote.js
@@ -19,7 +19,7 @@ async function getQuotes() {
             quoteAuthor.textContent=data[randomNum].author;
       }
       else if (keyQuote == "ru") { 
-            const quotes = '/quotes/quotes.json'
+            const quotes = 'quotes/quotes.json'
             const res = await fetch(quotes);
             const data = await res.json(); 
 
